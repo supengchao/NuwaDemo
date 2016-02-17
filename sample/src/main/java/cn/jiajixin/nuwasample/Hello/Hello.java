a4e2f5771c3b73cbc766d37507662dcb7c2b85b4
supengchao <1459384963@qq.com>
2016-02-17 16:32:01 +0800

    test

--- sample/src/main/java/cn/jiajixin/nuwasample/Hello/Hello.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/Hello/Hello.java
@@ -5,6 +5,8 @@ package cn.jiajixin.nuwasample.Hello;
  */
 public class Hello {
     public String say() {
-        return "hello bugfix";
+        return "hello world";
+    }
+    public void test(){
     }
 }
